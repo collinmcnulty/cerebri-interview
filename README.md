1678934acd5bb597e77d3841884e534cedd89c6a
Collin McNulty <collin.mcnulty@gmail.com>
2020-02-06 16:22:10 -0600

    Real starting point

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# cerebri
+# cerebri interview
+
+
+First, read parkinsons.names to learn about the data set. Run printSchema to verify data matches description
+
+
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import pyspark
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('ml-bank').getOrCreate()
-df = spark.read.csv('data/bank.csv',
+df = spark.read.csv('data/parkinsons.data',
                         header=True, inferSchema=True)
 df.printSchema()
