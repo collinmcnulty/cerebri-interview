c51961bdec0eb241c8871d2a73b1aed10dca1a43
Collin McNulty <collin.mcnulty@gmail.com>
2020-02-06 18:48:05 -0600

    Barebones random forest

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 First, read parkinsons.names to learn about the data set. Run printSchema to verify data matches description
 
 
+
+## Citations
+Max A. Little, Patrick E. McSharry, Eric J. Hunter, Lorraine O. Ramig (2008), 
+'Suitability of dysphonia measurements for telemonitoring of Parkinson's disease', 
+IEEE Transactions on Biomedical Engineering
--- test.py
+++ test.py
@@ -1,7 +1,34 @@
 import pyspark
+from pyspark.ml.feature import VectorAssembler, VectorIndexer, StringIndexer, IndexToString
 from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 spark = SparkSession.builder.appName('ml-bank').getOrCreate()
 df = spark.read.csv('data/parkinsons.data',
                         header=True, inferSchema=True)
-df.printSchema()
+
+labelIndexer = StringIndexer(inputCol="name", outputCol="label").fit(df)
+cols=['MDVP:Fhi(Hz)','MDVP:Flo(Hz)','MDVP:Jitter(%)']
+assembler_features = VectorAssembler(inputCols=cols, outputCol='features')
+
+print assembler_features
+
+# Split the data into training and test sets (30% held out for testing)
+(trainingData, testData) = df.randomSplit([0.7, 0.3])
+
+# Train a RandomForest model.
+rf = RandomForestClassifier(labelCol="status", featuresCol="features", numTrees=10)
+
+
+# Chain indexers and forest in a Pipeline
+pipeline = Pipeline(stages=[labelIndexer, assembler_features, rf])
+
+# Train model.  This also runs the indexers.
+model = pipeline.fit(trainingData)
+
+
+predictions = model.transform(df)
+
+predictions.show()
