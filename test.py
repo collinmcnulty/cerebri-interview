b158311d5f0132eb373913353e9289bbd97f0be7
Collin McNulty <collin.mcnulty@gmail.com>
2020-02-06 21:00:21 -0600

    muddle

--- test.py
+++ test.py
@@ -1,22 +1,30 @@
 import pyspark
 from pyspark.ml.feature import VectorAssembler, VectorIndexer, StringIndexer, IndexToString
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
 from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
+import pandas as pd
+data_path = __file__ + '/../data/parkinsons.data'
+pand = pd.read_csv(data_path)
+a = pand['name'].str.split('_', expand=True)[2].unique()
+
+
 
 spark = SparkSession.builder.appName('ml-bank').getOrCreate()
-df = spark.read.csv('data/parkinsons.data',
+df = spark.read.csv(data_path,
                         header=True, inferSchema=True)
 
+split_col = pyspark.sql.functions.split(df['name'], '_')
+df = df.withColumn('patient', split_col.getItem(2))
 labelIndexer = StringIndexer(inputCol="name", outputCol="label").fit(df)
 cols=['MDVP:Fhi(Hz)','MDVP:Flo(Hz)','MDVP:Jitter(%)']
 assembler_features = VectorAssembler(inputCols=cols, outputCol='features')
 
-print assembler_features
 
-# Split the data into training and test sets (30% held out for testing)
-(trainingData, testData) = df.randomSplit([0.7, 0.3])
+# Split the data into training and test sets (20% held out for testing)
+(trainingData, testData) = df.randomSplit([0.8, 0.2])
 
 # Train a RandomForest model.
 rf = RandomForestClassifier(labelCol="status", featuresCol="features", numTrees=10)
@@ -31,4 +39,23 @@ model = pipeline.fit(trainingData)
 
 predictions = model.transform(df)
 
+predictionAndLabels = testData.rdd.map(lambda lp: (float(model.predict(lp.features)), lp.status))
+metrics = BinaryClassificationMetrics(predictionAndLabels)
+
+# metrics = BinaryClassificationMetrics(predictions)
+def evaluate_results(predictions):
+    metrics = ['Precision', 'Recall', 'AUC']
+    values = []
+    metrics_evaluations = ['precision', 'recall', 'areaUnderROC']
+    evaluator = MulticlassClassificationEvaluator(predictionCol="prediction", labelCol="status")
+    for i, metric in enumerate(metrics_evaluations):
+        val = evaluator.setMetricName(metric).evaluate(predictions)
+        print val
+        values.append(val)
+    results = pd.DataFrame({'metric': metrics,
+                  'value': values})
+    return results
+
+results = evaluate_results(predictions)
+results.to_parquet('ml_test')
 predictions.show()
