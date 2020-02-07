b275b65a65683b3a66f94aa0a976283ec311644b
Collin McNulty <collin.mcnulty@gmail.com>
2020-02-06 21:53:37 -0600

    got metrics

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pyspark
 from pyspark.ml.feature import VectorAssembler, VectorIndexer, StringIndexer, IndexToString
-from pyspark.mllib.evaluation import BinaryClassificationMetrics
+from pyspark.mllib.evaluation import BinaryClassificationMetrics, MulticlassMetrics
 from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
@@ -18,7 +18,7 @@ df = spark.read.csv(data_path,
 
 split_col = pyspark.sql.functions.split(df['name'], '_')
 df = df.withColumn('patient', split_col.getItem(2))
-labelIndexer = StringIndexer(inputCol="name", outputCol="label").fit(df)
+labelIndexer = StringIndexer(inputCol="status", outputCol="label").fit(df)
 cols=['MDVP:Fhi(Hz)','MDVP:Flo(Hz)','MDVP:Jitter(%)']
 assembler_features = VectorAssembler(inputCols=cols, outputCol='features')
 
@@ -38,24 +38,28 @@ model = pipeline.fit(trainingData)
 
 
 predictions = model.transform(df)
+predictions.printSchema()
+# predictions = predictions.withColumnRenamed('status', 'label')
 
-predictionAndLabels = testData.rdd.map(lambda lp: (float(model.predict(lp.features)), lp.status))
-metrics = BinaryClassificationMetrics(predictionAndLabels)
-
-# metrics = BinaryClassificationMetrics(predictions)
-def evaluate_results(predictions):
-    metrics = ['Precision', 'Recall', 'AUC']
-    values = []
-    metrics_evaluations = ['precision', 'recall', 'areaUnderROC']
-    evaluator = MulticlassClassificationEvaluator(predictionCol="prediction", labelCol="status")
-    for i, metric in enumerate(metrics_evaluations):
-        val = evaluator.setMetricName(metric).evaluate(predictions)
-        print val
-        values.append(val)
-    results = pd.DataFrame({'metric': metrics,
-                  'value': values})
-    return results
-
-results = evaluate_results(predictions)
-results.to_parquet('ml_test')
-predictions.show()
+truncated = predictions.select(['prediction', 'label'])
+truncated.show()
+metrics = BinaryClassificationMetrics(truncated.rdd)
+m_metrics = MulticlassMetrics(truncated.rdd)
+1
+#
+# def evaluate_results(predictions):
+#     metrics = ['Precision', 'Recall', 'AUC']
+#     values = []
+#     metrics_evaluations = ['precision', 'recall', 'areaUnderROC']
+#     evaluator = MulticlassClassificationEvaluator(predictionCol="prediction", labelCol="status")
+#     for i, metric in enumerate(metrics_evaluations):
+#         val = evaluator.setMetricName(metric).evaluate(predictions)
+#         print val
+#         values.append(val)
+#     results = pd.DataFrame({'metric': metrics,
+#                   'value': values})
+#     return results
+#
+# results = evaluate_results(predictions)
+# results.to_parquet('ml_test')
+# predictions.show()
